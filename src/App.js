e301a9190e270a879679c2d139faeb7a763506f6
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2022-12-02 23:03:46 -0500

    MineCraft C app Git Update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef } from "react";
+import FileOutputLogic from "./FileOutputLogic";
 
 function App() {
+  const inputFileRef = useRef(null);
+  const contentTargetRef = useRef(null);
+
+  function getFile() {
+    if (inputFileRef.current === null);
+    else {
+      if (
+        "files" in inputFileRef.current &&
+        inputFileRef.current.files.length > 0
+      ) {
+        placeFileContent(
+          contentTargetRef.current,
+          inputFileRef.current.files[0]
+        );
+      }
+    }
+  }
+
+  function placeFileContent(target, file) {
+    readFileContent(file)
+      .then((content) => {
+        target.value = content;
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function readFileContent(file) {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(file);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Mine Craft Gen APP</h1>
+        <h2>Kitty Shizz's PreFab Convertor</h2>
+        <div className="fileInputContainer">
+          <label>Specify a file:</label>
+          <input type="file" onChange={getFile} ref={inputFileRef} />
+        </div>
+
+        <textarea ref={contentTargetRef} />
+
+        <h1>OutPut Files</h1>
+        <div className="outPutFileContainer">
+          <FileOutputLogic ContentTarget={contentTargetRef} />
+        </div>
       </header>
     </div>
   );
