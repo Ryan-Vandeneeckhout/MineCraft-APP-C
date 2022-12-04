a5511375b68f9a9dba22e80ce6d7c8bd4e9e2c9b
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2022-12-04 04:58:47 -0500

    Massive id update

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
       <header className="App-header">
         <div className="headerContainer">
           <div className="wordTitle">
-            <h1>Mine Craft Gen APP</h1>
+            <h1>Mine Craft Gen APP Version 2!</h1>
             <h2>Kitty Shizz's PreFab Convertor</h2>
           </div>
           <div className="imageTitle">
